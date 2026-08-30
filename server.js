5238962d4aaa997d8e386c516d5aae7878e50006
Avishka Isurusiri <avishkaisurusiri@gmail.com>
2026-08-31 00:04:48 +0530

    Fix analytics static file serving

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 
+
+const path = require("path");
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
@@ -22,7 +24,21 @@ app.use(
 );
 
 app.use(express.json());
-app.use(express.static("public"));
+app.use(
+  express.static(
+    path.join(__dirname, "public")
+  )
+);
+
+app.get("/", (req, res) => {
+  res.sendFile(
+    path.join(
+      __dirname,
+      "public",
+      "index.html"
+    )
+  );
+});
 
 
 app.get("/health", async (req, res) => {
